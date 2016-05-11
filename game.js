e62b099bd30f7e15b55db5cadaa3acff2444caa8
Kelvin Wong <kelvin55699@hotmail.com>
2016-05-11 09:32:39 -0700

    function added to generate random between 700 and 1300

--- game.js
+++ game.js
@@ -28,6 +28,10 @@ function generate() {
 
 setInterval(generate, 1000);
 
+function randomIntForInterval(min,max){
+    Math.floor(Math.random() * (601) + 700);
+}
+
 //Generate sprites depending on the number of tracks.
 function generateSprites(trackNum) {
     var sprite1 = $('<img src="circle.png" id="circle">');

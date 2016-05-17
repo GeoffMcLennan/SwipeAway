ec7b0eb44e4ed6a4013a75b3f94bdc09201a97cf
Jesse Goertz <jessegrtz@gmail.com>
2016-05-17 09:47:35 -0700

    fixed all dynamic rotation

--- scripts/menus.js
+++ scripts/menus.js
@@ -2,42 +2,28 @@ $.mobile.pushStateEnabled = false;
 
 //adds margin to menu screens based on orientation
 $(document).ready(function() {
-	var orientation = screen.orientation || screen.mozOrientation || screen.msOrientation;
-
-if (orientation === "landscape-primary") {
-  console.log("That looks good.");
-} else if (orientation === "landscape-secondary") {
-  console.log("Mmmh... the screen is upside down!");
-} else if (orientation === "portrait-secondary" || orientation === "portrait-primary") {
-  console.log("Mmmh... you should rotate your device to landscape");
-}
 	// Recalls intialize function if screen orientation is changed.
-	/* $(window).on("orientationchange", function(event){
-		margins();
+	$(window).on("orientationchange", function(event) {
+		refreshMargin();
 	});
-	margins(); */
-});
+	refreshMargin();
+});  		
 
-/* function margins() {
-	var orientation = screen.orientation;
-	
-		if (orientation === "landscape-primary") {
-  console.log("That looks good.");
-} else if (orientation === "landscape-secondary") {
-  console.log("Mmmh... the screen is upside down!");
-} else if (orientation === "portrait-secondary" || orientation === "portrait-primary") {
-  console.log("Mmmh... you should rotate your device to landscape");
-} */
-		
-		/* if (orientation === "portrait-primary" || orientation === "portrait-secondary") {
-			$('#title').css({"margin-top": "80px"});
-			$('#title').css({"margin-bottom": "80px"});
-			$('#subTitle').css({"margin-bottom": "140px"});
-		} else {
-			$('#title').css({"margin-top": "0"});
-			$('#title').css({"margin-bottom": "0"});
-			$('#subTitle').css({"margin-bottom": "20px"});
-		} */	
+
+
+function refreshMargin() {
+
+	if (window.innerHeight > window.innerWidth) {
+		$('#title').css({"margin-top": "80px"});
+		$('#title').css({"margin-bottom": "80px"});
+		$('#subTitle').css({"margin-bottom": "140px"});
+	} else {
+		$('#title').css({"margin-top": "0"});
+		$('#title').css({"margin-bottom": "0"});
+		$('#subTitle').css({"margin-bottom": "20px"});
+	}
+}
+
 
 
 

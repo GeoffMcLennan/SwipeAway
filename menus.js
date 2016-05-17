2f184f4f207b69a1776188ec2288dd67b596e1ee
Jesse Goertz <jessegrtz@gmail.com>
2016-05-17 00:09:08 -0700

    super mad i cant get this damn roation shit working

--- menus.js
+++ menus.js
@@ -2,15 +2,34 @@ $.mobile.pushStateEnabled = false;
 
 //adds margin to menu screens based on orientation
 $(document).ready(function() {
+	var orientation = screen.orientation || screen.mozOrientation || screen.msOrientation;
+
+if (orientation === "landscape-primary") {
+  console.log("That looks good.");
+} else if (orientation === "landscape-secondary") {
+  console.log("Mmmh... the screen is upside down!");
+} else if (orientation === "portrait-secondary" || orientation === "portrait-primary") {
+  console.log("Mmmh... you should rotate your device to landscape");
+}
 	// Recalls intialize function if screen orientation is changed.
-	$(window).on("orientationchange", function(event){
+	/* $(window).on("orientationchange", function(event){
 		margins();
 	});
-	margins();
+	margins(); */
 });
 
-function margins() {
-		if (window.orientation == 0 || window.orientation == 180) {
+/* function margins() {
+	var orientation = screen.orientation;
+	
+		if (orientation === "landscape-primary") {
+  console.log("That looks good.");
+} else if (orientation === "landscape-secondary") {
+  console.log("Mmmh... the screen is upside down!");
+} else if (orientation === "portrait-secondary" || orientation === "portrait-primary") {
+  console.log("Mmmh... you should rotate your device to landscape");
+} */
+		
+		/* if (orientation === "portrait-primary" || orientation === "portrait-secondary") {
 			$('#title').css({"margin-top": "80px"});
 			$('#title').css({"margin-bottom": "80px"});
 			$('#subTitle').css({"margin-bottom": "140px"});
@@ -18,7 +37,7 @@ function margins() {
 			$('#title').css({"margin-top": "0"});
 			$('#title').css({"margin-bottom": "0"});
 			$('#subTitle').css({"margin-bottom": "20px"});
-		}	
-}	
+		} */	
+
 
 	

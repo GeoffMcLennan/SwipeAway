b074cbbeb6344f5f5ae1788acfd04f191cfcc83d
GeoffMcLennan <geoff.d.mclennan@gmail.com>
2016-05-24 21:16:11 -0700

    Modified log out so that we don't need seperate page

--- lib/logout.php
+++ lib/logout.php
@@ -1,4 +1,13 @@
 <?php
-session_unset();
-session_destroy();
+	// Start Session
+	if (!isset($_SESSION)) { 	
+		session_start();
+	}
+
+	session_unset();
+	session_destroy();
+
+	require_once('config_local.php');
+
+	header('Location: ' . HOME . '/index.php');
 ?>

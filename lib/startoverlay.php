ff2f31845313f1981afe152646059b79b8a08b1c
Jesse Goertz <jessegrtz@gmail.com>
2016-05-26 20:39:35 -0700

    added tutorial picture

--- lib/startoverlay.php
+++ lib/startoverlay.php
@@ -5,7 +5,9 @@
 
 	<h2 class="innerText">Score needed: <span id="putScore"></span></h2>
 
-	<h3 id="tutorial">Swipe the red obstacles up or down</h3>
+	<div id="tutorial">
+		<img id="tutorial" src="images/tutorial.png">
+	</div>
 	
 	<div id="options">
 		<a class="button" id="quit" href="index.php" rel="external">Quit&nbsp</a>
